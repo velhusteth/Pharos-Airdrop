310442eb86d1a983e8adf057c0ae49f82e89a5ff
Velhust <148684729+velhusteth@users.noreply.github.com>
2025-05-21 11:58:39 +0700

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -4,15 +4,19 @@ export const sleep = (millis: number) => {
 
 export const wallets = [
   {
-    address1: "0xCA2f0C5aEf4Df89a90DCb52561B5c7BbFF01A817",
+    address1: "0xYourWalletAddressHere",
     private1:
       "Paste Private key vao day",
     proxy: "", // bỏ trống nếu không có proxy
   },
+{
+    address1: "0xYourWalletAddressHere",
+    private1: "YourPrivateKeyHere",
+    proxy: "", // bỏ trống nếu không có proxy
+  },
   {
-    address1: "0xA3E9fbfF456273A760976f369E7e31D3BA533333",
-    private1:
-      "e7be2bf487fc2c1c49fda12de803d222861a01abd9e8305b156713d53adb6713",
-    proxy: "http://username:passs@host:port", // thêm proxy nếu có
+    address1: "0xAnotherWalletAddressHere",
+    private1: "AnotherPrivateKeyHere",
+    proxy: "", // bỏ trống nếu không có proxy
   },
 ];
